4fa5a528b09f6eebefa300879e9d9532014513e2
candeortiz <candelaria@10.251.2.64>
2016-06-16 04:12:58 +0200

    Update night

--- src/lab12/RoshamboApp.java
+++ src/lab12/RoshamboApp.java
@@ -5,17 +5,60 @@ import java.util.Scanner;
 public class RoshamboApp {
 
 	public static void main(String[] args) {
-		System.out.println("Enter name:");
+		// for the inputs
 		Scanner scan = new Scanner(System.in);
-		String name = scan.nextLine();
+		// handles all the validation
+		Validator validator = new Validator();
+
+		// first get name
+		String name = "";
+		System.out.println("Enter name:");
+		name = scan.nextLine();
+
+		// Make a loop to control and eventl. repeat name
+		boolean contName = true;
+		while (contName) {
+			if (validator.isValidName(name)) {
+				// what happens if wrong
+				contName = false;
+			} else {
+				System.out.println("Please enter a valid name!: ");
+			}
+		}
 		System.out.println("Welcome, " + name + "!");
 
-		System.out.println("Please choose player 1 or player2");
-		String playerChoice = scan.nextLine();
+		// select the opponent
 
-		System.out.println("Choose rock, paper or scissors: ");
-		String roshamboChoice = scan.nextLine();
+		boolean contOpponent = true;
+		// control event. repeat
+		while (contOpponent) {
+			// while true choose
+			System.out.println("Please choose player 1 or player2: ");
+			String choicePlayer = scan.nextLine();
 
+			if (validator.isValidOpponent(choicePlayer)){
+				
+			// create the Opponent Player depending on the users choice
+
+		}
+		if (choicePlayer.equalsIgnoreCase("1")) {
+			// one possibility
+			// only 2 player opponent
+			// opponentPlayer = new PlayerRandom();
+
+		} else {
+			// the other possibility
+			// opponentPlayer = newPlayerRock(;)
+		}
+		contOpponent = false;
+		} else {
+			 System.out.println("Please enter 1 or 2!");
+		}
 	}
+
+	// now user choose his roshambo
+
+	// need a while loop
+	boolean contRoshambo = true;
 
 }
--- src/lab12/Validator.java
+++ src/lab12/Validator.java
@@ -2,4 +2,30 @@ package lab12;
 
 public class Validator {
 
+	public boolean isValidName(String name) {
+		// to check
+		return !name.isEmpty();
+	}
+
+	// to control opponents
+	// true or false --> to use it at loop
+	public boolean isValidOpponent(String choicePlayer) {
+		// only 2 or 1 allowed
+		return choicePlayer.equalsIgnoreCase("1")
+				|| choicePlayer.equalsIgnoreCase("2");
+
+	}
+
+	public boolean isValidRoshambo(String choiceRoshambo) {
+		// check if string is ok
+		// http://stackoverflow.com/questions/4936819/java-check-if-enum-contains-a-given-string
+
+		// Roshambo.values is in java offering me the option
+		for (Roshambo roshambo : Roshambo.values()) {
+			if (roshambo.name().equalsIgnoreCase(choiceRoshambo)) {
+				return true;
+			}
+		}
+	}
+
 }
